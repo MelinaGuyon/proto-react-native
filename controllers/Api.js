361bf83d02b3ea036dfb937a97baf44220fe6dc2
GUYON Mélina <mguyon@g-a101-32.local>
2018-03-02 14:47:07 +0100

    Change token

--- controllers/Api.js
+++ controllers/Api.js
@@ -1,7 +1,7 @@
 
 function connect() {
   return new Promise((resolve, reject) => {
-    fetch('https://api.afp.com/v1/api/latest?access_token=62c7146b-1546-4fff-9eaf-6f20cd775b31')
+    fetch('https://api.afp.com/v1/api/latest?access_token=7bf8a2c5-6c4b-4335-9c6d-9c25bf345b0a')
       .then(response => {
         if (response.status === 200) {
           resolve(response)
